1fa05e36c43777812943b39b1820fce5bb37e77a
Riley Mathews <rileymathews80@gmail.com>
2018-07-26 11:09:24 -0500

    modified payment model to be more detailed

--- Api/models/payment_model.py
+++ Api/models/payment_model.py
@@ -4,11 +4,15 @@ from .user_model import User
 
 class Payment(models.Model):
     name = models.CharField(max_length=50)
-    payment_type = models.CharField(max_length=400)
+    payment_type = models.Field.choices = (
+        ('CD', 'Card'),
+        ('BK', 'Bank Account')
+    )
     account_number = models.IntegerField()
     expiration = models.DateField()
     security_code = models.IntegerField()
     bank = models.CharField(max_length=20)
+    user_id = models.ForeignKey(User, related_name='payment', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.name}: {self.description}'
+        return f'{self.name}: {self.bank}'

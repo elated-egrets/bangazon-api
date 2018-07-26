b652cc539d22547ebe3e0d62fdce97d0efe4c8fc
pzc <paul.zimmerman.clayton@gmail.com>
2018-07-26 15:04:44 -0500

    first pass at product endpoint

--- Api/urls.py
+++ Api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import Category_Viewset, Payment_Viewset, User_Viewset
-from .views import Category_Viewset, User_Viewset, Order_Viewset
+from .views import Category_Viewset, Payment_Viewset, User_Viewset, User_Viewset, Order_Viewset, Product_Viewset
 from rest_framework.routers import DefaultRouter
 
 
@@ -9,4 +8,5 @@ router.register('category', Category_Viewset, base_name='Category')
 router.register('payment', Payment_Viewset, base_name='Payment')
 router.register('user', User_Viewset, base_name='user')
 router.register('order', Order_Viewset, base_name="order")
+router.register('product', Product_Viewset, base_name="product")
 urlpatterns = router.urls

242c1da3705a173d2ec1817120ca4ef58c6acb8c
Jonny-Riggs <jonny.riggs615@gmail.com>
2018-07-27 10:42:53 -0500

    Created docstrings for models and views

--- Api/models/category_model.py
+++ Api/models/category_model.py
@@ -2,8 +2,19 @@ from django.db import models
 
 
 class Category(models.Model):
-  name = models.CharField(max_length=50)
-  description = models.CharField(max_length=400)
+      '''Model for the categories.
 
-  def __str__(self):
-    return f'{self.name}: {self.description}'
+      Data:
+        name --- name representation of the category
+        description --- description of the category
+      '''
+      name = models.CharField(max_length=50)
+      description = models.CharField(max_length=400)
+
+      def __str__(self):
+        '''A string representation of the Category.
+
+        Returns:
+          string --- category name and then description of said category.
+        '''
+        return f'{self.name}: {self.description}'

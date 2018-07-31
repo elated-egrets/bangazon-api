366f19d6c04da31b5f00d9007d2c0cca677a3806
Riley Mathews <rileymathews80@gmail.com>
2018-07-31 09:28:33 -0500

    added docstring to order product model

--- Api/models/order_product_model.py
+++ Api/models/order_product_model.py
@@ -3,6 +3,13 @@ from .order_model import Order
 from .product_model import Product
 
 class Order_Product(models.Model):
+    """Model for order product intersection table
+        This model is used to ensure that the nested products are available to the order resource
+
+    fields:
+        order_id: the id of the order
+        product_id: the id of the product on the order
+    """
 
     order_id = models.ForeignKey(Order, related_name="order_intersection", on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, related_name="product_intersection", on_delete=models.CASCADE)
